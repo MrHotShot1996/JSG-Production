9656ae7d37d86545f8800d238964583c7cc561fe
unknown <hasanalboug@yahoo.com>
2021-06-07 05:03:53 +0300

    f

---
+++ keys.py
@@ -0,0 +1,13 @@
+# Django hidden variables
+
+# Secret Key
+sk = 'django-insecure-jbb1j+mg8o!f-q6ns8b681c15v)-!z6&khrsu2sm_q+oz_^o*4'
+
+# Email Host
+
+email_name = 'mydev1996@gmail.com'
+email_pass = 'zjHJs7L8mupx4gMd'
+
+admins = [
+    ("hasan", "hasanalboug@yahoo.com"),
+]
